2d949ad29469e28ab0ed520ebe9d3a07dc5345de
julio.oliveira <jcalvesoliveira@hotmail.com>
2020-12-13 20:42:23 -0300

    removing extra options

--- server/server.py
+++ server/server.py
@@ -28,37 +28,7 @@ class Server(object):
         logger.info("Creating server")
         logger.debug("ThreadPoolExecutor.max_workers = %s", GRPC_MAX_WORKERS)
         self.server = grpc.server(
-            futures.ThreadPoolExecutor(max_workers=GRPC_MAX_WORKERS),
-            options=self._server_options(),
-        )
-
-    def _server_options(self):
-        server_options = (
-            ("grpc.keepalive_time_ms",
-             20000),  # send keepalive every x milliseconds
-            (
-                "grpc.keepalive_timeout_ms",
-                5000,
-            ),  # keepalive ping time out after x milliseconds
-            (
-                "grpc.keepalive_permit_without_calls",
-                True,
-            ),  # allow keepalive pings when there's no gRPC calls
-            (
-                "grpc.http2.max_pings_without_data",
-                0,
-            ),  # allow unlimited amount of keepalive pings without data
-            (
-                "grpc.http2.min_time_between_pings_ms",
-                10000,
-            ),  # allow grpc pings from client every x milliseconds
-            (
-                "grpc.http2.min_ping_interval_without_data_ms",
-                5000,
-            ),  # allow grpc pings from client without data every x milliseconds
-        )
-        logger.debug("Server options: {}".format(server_options))
-        return server_options
+            futures.ThreadPoolExecutor(max_workers=GRPC_MAX_WORKERS))
 
     def _add_servicers(self):
         logger.info("Adding servicers")
